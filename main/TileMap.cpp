6ace4d585ce65c9558fa6439fb5bb19528a3dad3
KhDan007 <kh.dan007@gmail.com>
2022-12-05 14:39:43 +0600

    setup tilemap class

--- main/TileMap.cpp
+++ main/TileMap.cpp
@@ -1,2 +1,30 @@
 #include "stdafx.h"
 #include "TileMap.h"
+
+// Constutors / Destructors
+TileMap::TileMap()
+{
+	// Init tilemap
+}
+
+TileMap::~TileMap()
+{
+}
+
+// Functions
+void TileMap::addTile(unsigned x, unsigned y)
+{
+}
+
+void TileMap::removeTile(unsigned x, unsigned y)
+{
+}
+
+
+void TileMap::update()
+{
+}
+
+void TileMap::render()
+{
+}
